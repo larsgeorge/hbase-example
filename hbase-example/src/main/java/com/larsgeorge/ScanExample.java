16b14350cd436188c910e7f9e8635664a183973c
Lars George <lars@okera.com>
2022-11-02 16:04:40 +0100

    Added test for cluster connection.

--- hbase-example/src/main/java/com/larsgeorge/ScanExample.java
+++ hbase-example/src/main/java/com/larsgeorge/ScanExample.java
@@ -13,11 +13,13 @@ import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.RetriesExhaustedException;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.client.TableDescriptor;
 import org.apache.hadoop.hbase.client.TableDescriptorBuilder;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.zookeeper.ReadOnlyZKClient;
 
 import java.io.IOException;
 
@@ -32,11 +34,16 @@ public class ScanExample {
  */
 public static void main(String[] args) throws IOException {
     Configuration conf = HBaseConfiguration.create();
+    conf.set(HConstants.ZOOKEEPER_QUORUM, "107-21-193-76.compute-1.amazonaws.com");
+    conf.setInt(ReadOnlyZKClient.RECOVERY_RETRY, 2);
     try (
       Connection connection = ConnectionFactory.createConnection(conf);
       Table table = connection.getTable(TABLE_NAME);
     ) {
-      System.out.println("inside");
+      if (connection.getClusterId() == null) {
+        System.out.println("ERROR: Could not connect to HBase!");
+        System.exit(-1);
+      }
       Scan scan = new Scan();
       ResultScanner scanner = table.getScanner(scan);
       for (Result result2 : scanner) {
